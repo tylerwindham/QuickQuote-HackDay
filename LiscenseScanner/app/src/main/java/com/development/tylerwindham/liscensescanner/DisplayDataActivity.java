150e4794fa7841fdb317a57716cab8ddcf37dfa9
tylerwindham <tylerwindham47@gmail.com>
2017-05-11 22:08:38 -0500

    Initial Commit

---
+++ LiscenseScanner/app/src/main/java/com/development/tylerwindham/liscensescanner/DisplayDataActivity.java
@@ -0,0 +1,22 @@
+package com.development.tylerwindham.liscensescanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisplayDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_data);
+
+        Intent intent = getIntent();
+        String data = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = (TextView) findViewById(R.id.license_data);
+        textView.setText(data);
+    }
+}
