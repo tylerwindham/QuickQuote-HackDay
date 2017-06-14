5dd65c32a5a9d72ab8e3e28006255f62c320f68c
RyanSong <shoidoly75@gmail.com>
2017-06-14 09:31:15 -0500

    add entry page

---
+++ LiscenseScanner/app/src/main/java/com/development/tylerwindham/liscensescanner/EntryPageActivity.java
@@ -0,0 +1,13 @@
+package com.development.tylerwindham.liscensescanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EntryPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_page);
+    }
+}
