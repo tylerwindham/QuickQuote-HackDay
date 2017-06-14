28caa6a21666fcec00cc4d563e92fc7bc1439765
Dustin <dustryce@gmail.com>
2017-06-14 09:24:10 -0500

    Adding personalInfo and Address beans

---
+++ LiscenseScanner/app/src/main/java/com/development/tylerwindham/licenseObjects/Address.java
@@ -0,0 +1,36 @@
+package com.development.tylerwindham.licenseObjects;
+
+/**
+ * Created by Dustin on 6/14/2017.
+ */
+
+public class Address {
+
+    private String street;
+    private String zipCode;
+    private String state;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
---
+++ LiscenseScanner/app/src/main/java/com/development/tylerwindham/licenseObjects/PersonalInfo.java
@@ -0,0 +1,63 @@
+package com.development.tylerwindham.licenseObjects;
+
+/**
+ * Created by Dustin on 6/14/2017.
+ */
+
+public class PersonalInfo {
+
+    private String firstName;
+    private String lastName;
+    private String middleInitial;
+    private String licenseNumber;
+    private String dateOfBirth;
+    private Address address;
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public void setMiddleInitial(String middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
